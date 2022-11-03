d054e456b125427631ba7f5aecaaef047b26967d
ibrah100 <37915317+ibrah100@users.noreply.github.com>
2022-11-02 20:31:27 -0500

    finished stretch goals

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -89,6 +89,18 @@ if (pets < allowedPets) {
 // be sure to think through all the scenarios. 
 // console.log `mostPets` after the conditional has run.
 
+var mostPets;
+
+if (pets > friendsPets) {
+    pets = mostPets;
+} else if (pets = friendsPets) {
+    mostPets = "We have the same amount of pets!"
+} else {
+    friendsPets = mostPets;
+}
+
+console.log(mostPets);
+
 // 17 - Write a *switch* statement that logs:
 //      "First is the worst" if your lucky number is 1
 //      "Second is the best" if your lucky number is 2
@@ -96,6 +108,20 @@ if (pets < allowedPets) {
 //      Otherwise, log "Luck is what happens when preparation meets opportunity"
 //      You'll need to research how to use switch statements!
 
+switch (luckyNumber) {
+    case 1:
+        console.log("First is the worst");
+        break;
+    case 2:
+        console.log("Second is the best");
+        break;
+    case 3:
+        console.log("Third is the one with the polka dot dress")
+    default:
+        console.log("Luck is what happens when preparation meets opportunity")
+}
+
 // 18 -- Rewrite question 13 with a `ternary` operator. You'll need to do research!
 
+console.log((adventuruous = true) ? "Adventures are great!" : "How about we stay home?");
 
